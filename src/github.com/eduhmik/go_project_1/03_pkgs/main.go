44c72236e3216695da24191aaeccbc6fe6fcd161
eduhmik <edwinkimaita78@gmail.com>
2019-10-02 15:23:50 +0300

    added a custom package to reverse a string

---
+++ src/github.com/eduhmik/go_project_1/03_pkgs/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"github.com/eduhmik/go_project_1/03_pkgs/strutil"
+)
+
+
+func main()  {
+	fmt.Println(math.Floor(2.89))
+	fmt.Println(math.Ceil(2.89))
+	fmt.Println(math.Sqrt(2.89))
+	fmt.Println(strutil.Reverse("olleh"))
+}
