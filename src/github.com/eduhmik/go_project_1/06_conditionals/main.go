7eadf838df009d09e4bb6e10f4cb9071ae4ac01f
eduhmik <edwinkimaita78@gmail.com>
2019-10-03 12:43:51 +0300

    if else, switch and do while conditional statements in go

--- src/github.com/eduhmik/go_project_1/06_conditionals/main.go
+++ src/github.com/eduhmik/go_project_1/06_conditionals/main.go
@@ -1,15 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func main()  {
+func main() {
 
 	x := 6
 	y := 7
 
-	if x % y == 0 {
+	// if else
+
+	if x%y == 0 {
 		fmt.Printf("%d is a divisor of %d\n", y, x)
 	} else {
 		fmt.Printf("%d is not a divisor of %d\n", y, x)
+	}
+
+	// else if
+
+	color := "red"
+
+	if color == "red" {
+		fmt.Println("There is danger")
+	} else if color == "green" {
+		fmt.Println("Go on")
+	} else {
+		fmt.Println("Invalid color")
+	}
+
+	// switch
+
+	switch color {
+	case "red":
+		fmt.Println("There is danger. Stop!!!")
+	case "green":
+		fmt.Println("Drive on")
+	default:
+		fmt.Println("Does not recognize the value")
+	}
+
+	// do while
+
+	var z = 6
+
+	for {
+		z--
+		if z == 0 {
+			break
+		}
+		fmt.Println("Counting...", z)
+		if z == 1 {
+			fmt.Println("Fireeeee!!!!")
+		}
 	}
 }
