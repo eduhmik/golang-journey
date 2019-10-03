bb4256fe2607158953f5744035103a7e8598b6b0
eduhmik <edwinkimaita78@gmail.com>
2019-10-03 13:08:34 +0300

    fizzbuzz challenge

---
+++ src/github.com/eduhmik/go_project_1/07_loops/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	// for loop
+	for i := 1; i <= 10; i++ {
+		// fmt.Println(i)
+	}
+
+	// FizzBuzz
+	for k := 1; k <= 100; k++ {
+		if k % 15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if k % 3 == 0 {
+			fmt.Println("Fizz")
+		} else if k % 5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(k)
+		}
+	}
+}
