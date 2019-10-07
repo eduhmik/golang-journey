5f93eb4006938e899f9a9649c957faefd5a5b67d
eduhmik <edwinkimaita78@gmail.com>
2019-10-07 16:06:47 +0300

    pointer and how to read values from address

---
+++ src/github.com/eduhmik/go_project_1/10_pointers/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+
+	// pointer allows you to point to a memory address of a value
+	a := 5
+	b := &a
+
+	fmt.Println(a, b)
+	fmt.Printf("%T\n", b)
+
+	// use * to read val from address
+	fmt.Println(*b)
+}
