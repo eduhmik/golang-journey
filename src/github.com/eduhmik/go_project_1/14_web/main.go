5d31ecc85ad6e9c563b0b22d2f2fc525fd950f93
eduhmik <edwinkimaita78@gmail.com>
2019-10-10 18:57:12 +0300

    commenting the code

--- src/github.com/eduhmik/go_project_1/14_web/main.go
+++ src/github.com/eduhmik/go_project_1/14_web/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "<h1>Hello World</h1>")
 }
 func main()  {
 	http.HandleFunc("/", index)
